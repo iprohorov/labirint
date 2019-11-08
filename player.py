b145c4bc8e231d99a18c9bb006b19c4e62e1dd93
Prohorov <prohorov.vla@yandex.by>
2019-11-08 19:43:24 +0300

    test add new animation

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
-import pygame 
+import pygame
+import pygame.transform
 
 def LoadImageList (fileNamesList):
     imageList = []
@@ -6,15 +7,17 @@ def LoadImageList (fileNamesList):
         imageList.append(pygame.image.load(fileName).convert_alpha())
     return imageList
 
-
-
 class Animation:
-    def __init__ (self,framesFiles, frameTime):
+    def __init__ (self,framesFiles, frameTime, flipX = False, flipY = False):
         self.frames = LoadImageList (framesFiles)
         self.lastUpdateAnimationTime = 0
         self.currentFrame = 0
         self.frameTime = frameTime
         self.isPlay = False
+        self._Transform(flipX, flipY)
+    def _Transform(self, flipX = False, flipY = False):
+        for i, frame in enumerate(self.frames):
+            self.frames[i] = pygame.transform.flip(frame, flipX, flipY)
     def isNeedApdate(self):
         if (pygame.time.get_ticks()-self.lastUpdateAnimationTime)>self.frameTime:
             self.currentFrame +=1
@@ -37,8 +40,11 @@ class Animation:
 class Player (pygame.sprite.Sprite):
     def __init__ (self, X=32, Y=32, cameraPositionX = 0, cameraPositionY = 0):
         pygame.sprite.Sprite.__init__(self)
-        self.upGoAnimation = Animation(["hero1z.png","hero2z.png","hero3z.png","hero4z.png"],500)
-        self.currentAnimation = self.upGoAnimation
+        #self.upGoAnimation = Animation(["hero1z.png","hero2z.png","hero3z.png","hero4z.png"],100)
+        self.downGoAnimation = Animation(["herou1.png","herou2.png","herou3.png","herou4.png"],100)
+        self.rightGoAnimation = Animation(["herol1.png","herol2.png","herol3.png","herol4.png"],100)
+        self.leftGoAnimation = Animation(["herol1.png","herol2.png","herol3.png","herol4.png"],100, True)
+        self.currentAnimation = self.downGoAnimation
         self.image = self.currentAnimation.getImg()
         self.rect = self.image.get_rect(center=(X,Y))
         self.x_speed = 0
@@ -65,16 +71,23 @@ class Player (pygame.sprite.Sprite):
         self.rect.y = self.y
 
     def MoveLeft (self):
+        self.currentAnimation = self.leftGoAnimation 
+        self.currentAnimation.Start()
         self.x_speed = -0.1
          
     def MoveRight (self):
+        self.currentAnimation = self.rightGoAnimation 
+        self.currentAnimation.Start()
         self.x_speed = 0.1
         
     def MoveUp (self):
-        self.currentAnimation.Start()
+        # self.currentAnimation = self.upGoAnimation
+        # self.currentAnimation.Start()
         self.y_speed = -0.1
         
     def MoveDown (self):
+        self.currentAnimation = self.downGoAnimation 
+        self.currentAnimation.Start()
         self.y_speed = 0.1
         
         
