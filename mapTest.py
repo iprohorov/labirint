3e94b3a99d05b30740049ea2086d1ce30a690397
Прохоров Владимир <iprohorov2319@gmail.com>
2020-06-13 13:06:53 +0300

    title

--- mapTest.py
+++ mapTest.py
@@ -12,21 +12,21 @@ from pytmx import TiledObjectGroup
 #screen = pygame.display.set_mode(size,pygame.FULLSCREEN)
 screen = pygame.display.set_mode(size)
 tiled_map = pytmx.load_pygame('image\\Tilemap\\t1.tmx')
+print(tiled_map.get_tile_properties_by_gid(1))
 
+# for layer in tiled_map.visible_layers:
+#     print(layer)
+#     if isinstance(layer, TiledTileLayer):
+#         print("TiledTileLayer")
+#         print(TiledMap)
+#         for t in layer.iter_data():
+#             print(t)
+#     elif isinstance(layer, TiledObjectGroup):
+#         print("TiledObjectGroup")
+#     elif isinstance(layer, TiledImageLayer):
+#         print("TiledImageLayer")
 
-for layer in tiled_map.visible_layers:
-    print(layer)
-    if isinstance(layer, TiledTileLayer):
-        print("TiledTileLayer")
-        print(TiledMap.get_tile_properties(0, 0, layer))
-        for t in layer.iter_data():
-            print(t)
-    elif isinstance(layer, TiledObjectGroup):
-        print("TiledObjectGroup")
-    elif isinstance(layer, TiledImageLayer):
-        print("TiledImageLayer")
-
-pygame.display.flip()
+# pygame.display.flip()
 
 # while True:
 #     pass
